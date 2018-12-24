f08ab4db4b6d9af3899c43f72a1df3d5c967475b
Stegura Alex <stegura.alex@gmail.com>
2018-12-24 22:40:51 +0300

    first commit variebles

---
+++ variables/main.js
@@ -0,0 +1,26 @@
+// task 1
+
+const x = 2;
+const y = 22;
+const z = 0;
+
+const result = x * y + 5 * z + x - 1;
+
+console.log(`Ответ - ${result}`);
+
+// task 2
+
+const algebra = 5;
+const geography = 4;
+const physics = 4;
+
+const average = (algebra + geography + physics) / 3;
+
+console.log(`Средняя оценка - ${average} балла`);
+
+// task 3
+
+const name = 'Алексей';
+const message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
